3f1440cace8c75d8b3d4a4c712a6c646c180c8f2
Cagil Emek Kurtul <ckurtul@k1m09s01.42istanbul.com.tr>
2026-03-13 17:09:21 +0300

    adresler eklendi

--- ft_printf.c
+++ ft_printf.c
@@ -6,44 +6,136 @@
 /*   By: ckurtul <ckurtul@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 00:52:21 by ckurtul           #+#    #+#             */
-/*   Updated: 2026/02/19 07:08:01 by ckurtul          ###   ########.fr       */
+/*   Updated: 2026/03/12 17:09:53 by ckurtul          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include<stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
 
-void ft_putchar_fd(char c, int fd)
+int ft_putchar (int c)
 {
-	write(1, "c", 1);
+	write(1, &c, 1);
+	return 1;
 }
 
-void ft_putstr_fd(char *s, int fd)
+void ft_putnbr (int n)
+{
+	if(n<0)
+	{
+		ft_putchar('-');
+		n = -n;
+	}
+	if(n>=10)
+	{
+		ft_putnbr(n/10);
+	}
+	ft_putchar(n % 10 + '0');
+}
+
+void ft_uns_putnbr(int b)
+{
+	if (b >= 10)
+		ft_uns_putnbr(b / 10);
+	ft_putchar(b % 10 + '0');
+}
+
+void ft_hex_putnbr (int k)
+{
+	char *base = "0123456789abcdef";
+
+	if(k >= 16)
+		ft_hex_putnbr(k / 16);
+	ft_putchar(base[k % 16]);
+}
+
+void ft_uphex_putnbr (int k)
+{
+	char *base = "0123456789ABCDEF";
+
+	if(k >= 16)
+		ft_uphex_putnbr(k / 16);
+	ft_putchar(base[k % 16]);
+}
+
+
+int ft_printf(const char *format, ...)
 {
 	int a;
-	a = 0;
-	while (s[a])
+	va_list ar;
+
+	va_start(ar, format);
+
+	while (*format)
 	{
-		ft_putchar_fd(s[a], fd);
-		a++;
+		if(*format == '%')
+		{
+			format++;
+			{
+				if (*format == 'd' || *format == 'i')
+				{
+					a = va_arg(ar, int); // argümanı al
+					ft_putnbr(a);         // ekrana yaz
+				}
+				else if (*format == 'c')
+				{
+					a = va_arg(ar, int);
+					ft_putchar(a);
+				}
+				else if (*format == 's')
+				{
+					char *string;
+					string = va_arg(ar, char *);
+					while (*string)
+					{
+						ft_putchar (*string);
+						string++;
+					}
+				}
+				else if (*format == 'u')
+				{
+					unsigned int b;
+					b = va_arg(ar, unsigned int);
+					ft_uns_putnbr(b);
+				}
+				else if(*format == 'x')
+				{
+					unsigned int m;
+					m = va_arg(ar, unsigned int);
+					ft_hex_putnbr(m);
+				}
+				else if (*format == 'X')
+				{
+					unsigned int h;
+					h = va_arg(ar, unsigned int);
+					ft_uphex_putnbr(h);
+				}
+				else if (*format == '%')
+				{
+					int a;
+					a = '%';
+					ft_putchar(a);
+				}
+				else if(*format == 'p')
+				{
+					
+				}
+				format++;
+			}
+		}
+		else
+		{
+			ft_putchar(*format);
+			format++;
+		}
 	}
+
+	va_end(ar);
+	return 0;
 }
-void ft_putnbr_fd(int n, int fd)
+int main()
 {
-	
+	int *p = NULL;
+	ft_printf("%d", ft_printf("merhaba"));
 }
-int ft_printf(const char *n, ...)
-{
-	va_list list;
-	int c;
-	char a;
-
-	va_start (list, n);
-	c = va_arg(list, int);
-	a = va_arg(list, char);
-
-	va_end (list);
-
-	ft_printf("a", 's');
-	
-}
-
